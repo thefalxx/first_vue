27c3321bad6614a959335a11cb9f636d9b2890f9
unknown <forcetonxzx@gmail.com>
2023-05-17 14:13:34 +0800

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <h1 class="text-xl font-bold mb-2">Notes</h1>
+    <p class="mb-4">built with nuxt 3</p>
+    <ul class="border shadow divide-y">
+      <li v-for="note of filteredNotes" :key="note.id">
+        <NuxtLink :to="`${note.id}`" class="flex flex-col hover:bg-gray-50 p-3">
+          <span class="font-bold text-lg">{{ note.title }}</span>
+          <span>completed: {{ note.completed }}</span>
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
